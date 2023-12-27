43296163065d9ddace86793331c2b7848721c758
Thiago Silva de Oliveira <thiagodevoliveira@hotmail.com>
2023-12-27 20:40:06 -0300

    Exercicios aula 14

--- pythonExercicios/ex060.py
+++ pythonExercicios/ex060.py
@@ -1,14 +1,11 @@
 # Fatorial
 
-print("Digite um número para saber o seu fatorial:\n")
-fator = 0
-i = 2
-while i > 1:
-    num = int(input("Digite um número para saber o seu fatorial:\n"))
-    i = num
-    for c in range(num, 1, -1):
-        i -= 1
-        cont = i * (i - 1)
-        fator = cont * (i - 2)
+numero = int(input("Digite um número para obter o fatorial:\n"))
+resultado=1
+cont=1
 
-print(fator)
+while cont <= numero:
+    resultado *= cont
+    cont += 1
+
+print(resultado)
