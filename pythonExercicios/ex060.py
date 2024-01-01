4980e2cbebb9a126c62b636f269280db3cdff148
Thiago Silva de Oliveira <thiagodevoliveira@hotmail.com>
2024-01-01 17:24:43 -0300

    Alterando chave ssh

--- pythonExercicios/ex060.py
+++ pythonExercicios/ex060.py
@@ -1,11 +1,12 @@
 # Fatorial
 
 numero = int(input("Digite um número para obter o fatorial:\n"))
-resultado=1
-cont=1
+resultado = 1
+cont = 1
 
 while cont <= numero:
     resultado *= cont
     cont += 1
 
 print(resultado)
+# 1*5, 2 * 5, 10*3, 30*4, 120*5
