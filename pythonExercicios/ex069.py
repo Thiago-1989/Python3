63fccc213dfd7954aca2a499a634441137e83ac3
Thiago Silva de Oliveira <thiagosoaresccb@gmail.com>
2024-01-10 00:20:31 -0300

    Ex aula15

--- pythonExercicios/ex069.py
+++ pythonExercicios/ex069.py
@@ -1,22 +1,26 @@
 # Leia idade e sexo, perguntando se continua, e diga quantas pessoas são > 18 anos, quantos homens cadastrados e mulheres < 20 anos
-maior18 = 0
-homens = 0
-mulherMenos20 = 0
-
+maior18 = homens = mulherMenos20 = 0
 while True:
-    idade = int(input("Idade: \n"))
-    sexo = input("Sexo [M/F]: \n").strip().capitalize()
-    sexo = sexo[0]
-    conti = input("Quer continuar [S/N]? \n").strip().capitalize()
-
+    tipo = " "
+    sexo = " "
+    continuar = " "
+    idade = -1
+    print("-=-" * 20)
+    print("Cadastre uma pessoa.")
+    print("-=-" * 20)
+    idade = int(input("Idade:   "))
+    while sexo not in 'MF':
+        sexo = str(input("Sexo [M/F]:  ")).strip().upper()[0]
+    print("-=-" * 20)
+    while continuar not in 'SN':
+        continuar = str(input("Quer continuar [S/N]?   ")).strip().upper()[0]
     if idade >= 18:
         maior18 += 1
     if sexo == "M":
         homens += 1
     elif sexo == "F" and idade <= 20:
         mulherMenos20 += 1
-    if conti == "N":
+    if continuar == "N":
         break
-
 print(f"{maior18} pessoa(s) são maior(es) de 18 anos, {homens} homem(homens) cadastrados "
       f"e {mulherMenos20} mulher(mulheres) tem 20 anos ou menos.")
