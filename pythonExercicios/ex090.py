86aac8d5ef7cec00f6377934ce383fb1b294ab12
Thiago Silva de Oliveira <thiagosoaresccb@gmail.com>
2024-04-08 17:24:26 -0300

    Correção aula 19

--- pythonExercicios/ex090.py
+++ pythonExercicios/ex090.py
@@ -1,9 +1,12 @@
 dicio = {}
 dicio["Nome"] = str(input("Nome: ")).strip().capitalize()
 dicio["Média"] = float(input("Média: "))
-if dicio["Média"] > 5:
+if dicio["Média"] >= 7:
     dicio["Situação"] = "Aprovado"
+elif dicio["Média"] >= 5:
+    dicio["Situação"] = "Recuperação"
 else:
     dicio["Situação"] = "Reprovado"
+print("-=" * 20)
 for key, value in dicio.items():
-    print(f"{key} é igual a {value}")
+    print(f"  -{key} é igual a {value}")
